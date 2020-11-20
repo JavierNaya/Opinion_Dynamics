021600d91378d4872c233264c014b9ca63597622
duylailqd <33722325+duylailqd@users.noreply.github.com>
2020-11-21 00:42:18 +0100

    Add files via upload

--- coevol.m
+++ coevol.m
@@ -1,12 +1,14 @@
+close all;
+
 k = 4;
 gamma = 10;
-phi = 1;
+phi = 0.75;
 
 N = 100;   % number of people
 M = round(N*k/2);
 G = round(N/gamma);
 
-x = randi(G,N,1);   % range of opinions
+x = randi(G,N,1);   % assigne opinions to people
 m = zeros(N);   % symmetric matrix specifying edges between nodes
 
 l = M;    %   initiate randomly the edges
@@ -21,7 +23,7 @@ while l > 0
 end
 
 evo = x;   % evolution data
-t_end = 1e3;
+t_end = 5e3;
 
  for t = 1:t_end
      i = randi(N);
@@ -40,25 +42,41 @@ t_end = 1e3;
         end
         if rand <= phi && isempty(h) == 0
             q = randi(length(c));
-            j = randi(length(h));
-            if m(i,h(j)) == 0
-                m(i,c(q)) = 0;
-                m(c(q),i) = 0;
-                m(i,h(j)) = 1;
-                m(h(j),i) = 1;
-            else
-                t = t-1;
+            hh = h;
+            s = 0;
+            while s == 0 && sum(hh) > 0
+                j = randi(length(hh));
+                while hh(j) == 0
+                    j = randi(length(hh));
+                end
+                if m(i,hh(j)) == 0
+                    s = 1;
+                else
+                    hh(j) = 0;
+                end
             end
+            m(i,c(q)) = 0;
+            m(c(q),i) = 0;
+            m(i,h(j)) = 1;
+            m(h(j),i) = 1;
         else
-            j = randi(length(c));
+            s = 0;
+            while s == 0
+                j = randi(length(c));
+                if rand <= 1.666*abs(1/(evo(i,1)-x(c(j)))) && rand <= sum(m(c(j),:))/M
+                    s = 1;
+                end
+            end
             x(i) = x(c(j));
         end
+      else
+          t = t-1;
       end
       evo = [evo x];
  end
  
  figure
- hist(evo(:,t_end))
+ histogram(evo(:,t_end))
  
 %  figure
 %  hold on
